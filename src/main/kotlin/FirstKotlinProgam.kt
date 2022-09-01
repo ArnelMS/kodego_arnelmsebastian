033eaa8a1b893523d606f719e85455b1f93a204f
Arnel Sebastian <arnelmsebastian@gmail.com>
2022-09-01 20:59:07 +0800

    second commit

--- src/main/kotlin/FirstKotlinProgam.kt
+++ src/main/kotlin/FirstKotlinProgam.kt
@@ -4,6 +4,7 @@ author: Arnel
 Date Created: 09/01/2022
 Filename: FirstKotlinProgman.kt
 */
-print ("Arnel")
-print ("Sebastian")
+print ("Hello World")
+print ("Hello World!!!")
+    print ("Hellow World!!!!!!!!!")
 }
