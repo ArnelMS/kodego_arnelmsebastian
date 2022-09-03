fb543e6c0e6b78b3f6273418cf108217b76b3ad2
Arnel Sebastian <arnelmsebastian@gmail.com>
2022-09-03 09:17:01 +0800

    Sum Program

--- src/main/kotlin/FirstKotlinProgam.kt
+++ src/main/kotlin/FirstKotlinProgam.kt
@@ -4,10 +4,15 @@ author: Arnel
 Date Created: 09/01/2022
 Filename: FirstKotlinProgman.kt
 */
-print ("Hello World")
-print ("Hello World!!!")
-    print ("Hello World!!!!!!!!!")
-    print ("Hello World4")
-    print ("Hello World5")
-    print ("Hello World6")
+    println("Enter Number 1:")
+    var number1 : Int = readln().toInt()
+
+    println("My Number 2 is $number1")
+
+    println("Enter Number 2:")
+    var number2 : Int = readln().toInt()
+
+    println("My Number 2 is $number2")
+    var sum : Int = number1 + number2
+    println("The sum is $sum")
 }
