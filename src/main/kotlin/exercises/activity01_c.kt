6df8264c87596963c2e5a6636ce2525579def775
Arnel Sebastian <arnelmsebastian@gmail.com>
2022-09-03 14:26:24 +0800

    1st Commit

---
+++ src/main/kotlin/exercises/activity01_c.kt
@@ -0,0 +1,20 @@
+package exercises
+
+fun main(){
+    //Activity 01 - C
+
+    println("Enter your Full Name:")
+    var fullname1 : String = readln().toString()
+    println("Enter your Course:")
+    var course1 : String = readln().toString()
+    println("Enter your Email:")
+    var email1 : String = readln().toString()
+    println("Enter your Mobile #:")
+    var mobile1 : Int = readln().toInt()
+
+    println("FullName: $fullname1")
+    println("Course: $course1")
+    println("Email: $email1")
+    println("Mobile: $mobile1")
+
+}
