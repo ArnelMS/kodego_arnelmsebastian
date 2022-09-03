c3132752e72d5a23d40c7daefc72cfee97e2e046
Arnel Sebastian <arnelmsebastian@gmail.com>
2022-09-03 10:53:47 +0800

    1st Commit

---
+++ src/main/kotlin/KotlinProgram3.kt
@@ -0,0 +1,21 @@
+fun main (){
+    var number1 : Int = 21
+    var number2 : Int = 21
+
+    var answer : Int = number1 % number2
+    println("Number is $answer")
+
+    //complex operations
+    var complexformula: Int =(2*10)*(10/2)
+    println (complexformula)
+
+    //comparison
+
+    var expression : Boolean = (number1 < number2)
+    println(expression)
+
+    //increment operator
+    var incrementNum : Int = --number1
+    println(incrementNum)
+
+}
