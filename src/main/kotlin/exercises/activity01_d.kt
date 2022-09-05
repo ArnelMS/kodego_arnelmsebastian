718ef2b988c42b14103b3c2b43eae1da467ba01d
Arnel Sebastian <arnelmsebastian@gmail.com>
2022-09-05 16:41:31 +0800

    sept5_commit

---
+++ src/main/kotlin/exercises/activity01_d.kt
@@ -0,0 +1,12 @@
+fun main(){
+
+//Activity 01 - C
+
+    print("Please enter amount: ")
+    var amount : Int = readln().toInt()
+    println("$amount")
+
+    println("Do you want to add another amount?")
+    var addamount : Int = readln().toInt()
+    println("$addamount")
+}
