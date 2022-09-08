536f2e81869d3a7cfe02d186a908821ca37ee1ac
Arnel Sebastian <arnelmsebastian@gmail.com>
2022-09-08 12:59:20 +0800

    sept8 1st commit

--- src/main/kotlin/exercises/activity01_d.kt
+++ src/main/kotlin/exercises/activity01_d.kt
@@ -1,6 +1,3 @@
-import java.security.KeyStore.TrustedCertificateEntry
-import javax.swing.text.StyledEditorKit.BoldAction
-
 fun main() {
 
 //Activity 01 - D
@@ -12,80 +9,41 @@ fun main() {
 // It will only accept an Integer as input.
 //The total of the first input will be divided by the second input.
 
-    //ENTER AMOUNT
-    print("Please enter amount: ")
+    print("Please Enter amount: ")
     var amount: Int = readln().toInt()
-    var answer: String = "yes"
+    var condition: Boolean = true
 
-    while (answer == "yes") {
+    while (condition){
+        print("Do you want to add another amount? ")
+        var answer: String = readln().uppercase()
 
-        //ADD NEW AMOUNT?
-        print("Do you want to add another amount?")
-        answer = readln()
-        // YES OR NO
-        if (answer == "Yes" || answer == "yes") {
+        if (answer == "YES") {
             print("Enter new amount: ")
             var newamount: Int = readln().toInt()
             amount = amount + newamount
+
+            println("Sub-total is $amount")
+            println()
+
+        } else if (answer != "NO") {
+            println("Please answer with YES or NO only")
+            println()
+
+        } else if (answer == "NO") {
+            print("Sub-total is $amount")
+            println()
+
+            println("Divide by how many? ")
+            var divisor: Int = readln().toInt()
+
+            var total: Int = amount / divisor
+            println("Total :  $total")
+
+
+            println()
+            println("Thank you & Have a great day!!!")
+            break
         }
     }
-    println("Total amount is: $amount")
 
-    print("Divide by how many? ")
-    var divisor: Int = readln().toInt()
-
-    var total: Int = amount / divisor
-    println("Total :  $total")
-
-    println()
-    println("Thank you")
 }
-
-// WORKING -----------------------
-//        }
-//        //IF YES
-//        if (answer != "No" && answer != "no") {
-//            for (New in 1..5) {
-//                print("Enter new amount: ")
-//                var newamount: Int = readln().toInt()
-//                amount = amount + newamount
-//                println("Do you want to add another amount?")
-//                var answer: String = readln().toString()
-//                var condition: Boolean = false
-//            }
-//
-
-
-//
-//            // ----------------------------------------------------------------
-//            var yes: Any = "Yes"
-//            var no: Any = "No"
-//
-//            var ctr: Int = readln().toInt()
-//
-//            //condition
-//            while (ctr <= 5) {
-//                if (ctr == 3)
-//                    println("Hello World! $ctr")
-//                //update
-//                ctr++
-//
-//                for (New in 1..5) {
-//                    print("Enter new amount: ")
-//                    var newamount: Int = readln().toInt()
-//                    amount = amount + newamount
-//
-//                }
-//                println("Total amount is: $amount")
-//
-//                println("Divide by how many?")
-//                var divisor: Int = readln().toInt()
-//
-//                println(amount / divisor)
-//
-//                println()
-//            }
-//        }
-//    }
-//}
-//
